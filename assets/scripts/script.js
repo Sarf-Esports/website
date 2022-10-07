352da0fa240b7ad34af009e4153ff9e3a4159357
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-10-07 10:12:00 +0900

    Delete:og-image Rename:TITLE section to MEMBER Add:CONTACT section / Sarf Esports AA for the console

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,38 @@
-window.onload = function() {
-    console.log("Script fucking works!"); // TODO: Fuck this trash
-};
+console.log(`
+                                     -*ff
+                                            :f*++
+                              f              ffff=
+                          ffff
+                         ffffe   SSSSS
+                          eSSSSSSSSSSSSSSSS
+                 *        SSSSSSSSSSSSSSSSSSS
+                        SSSSSSSSS    .SSSSSSSSS
+                 +fffeSSSSSSS             SSSSSS:
+              .fffffeSSSSSSSe+             =SSSSSfff
+             =ffffffSSSSSSSfffeSSSSSSSS      SSefffff
+           ffffffffSSSSSSSf  SSSSSSSSSSSS    ffffffff*
+          fffffffffSSSSSSefeSSSSSSSSSSSSSS=   eeSeee       fffff
+           :ffffffeSSSSSSffSSSSSSS    SSSSS    SSSSS   *fffffffff
+              ffffeSSSSS  .SSSSSSf*   SSSSS=   SSSSS   .     ffff.
+                   SSSSS   *SSSSSefffffeSSSSffSSSSSS-         f   f
+                   SSSSS    SSSSSSSeeeffffeeffSSSSSSff             f
+                   =SSSSS    SSSSSSSSSSS:   feSSSSef
+     f        .     SSSSS     SSSSSSSSSSS    eeeffff             *
+                     SSSSS      eSefffffff-*fffffffff
+               fff   SSSSSSS       fffffffffffffffff.
+             fffffffffSSSSSSSS        fffffffffffff
+          fffffffffffffeSSSSSSSSS     SSeSS=+fff
+             fffffffffffffeeSSSSS    +SSSSS
+                  ffffffffffffffeSSSSSSSSS
+                      =ffffffffffffeeSS+
+                               ffffffff**
+
+ ###                   ###     #####                               #
+#   #                 #   #    #                                   #
+#       ###   # ###  ####      #       ####  # ##    ###   # ###  ####    ####
+ ###   #   #  ##      #        #####  #      ##  #  #   #  ##      #     #    
+    #   ####  #       #        #       ###   ##  #  #   #  #       #      ###
+#   #  #   #  #       #        #          #  # ##   #   #  #       #         #
+ ###    ####  #       #        #####  ####   #       ###   #        ##   ####
+                                             #
+`);
