8b5b6392b0abf231cfc84e48fa5ebbb2d4e575c7
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-10-07 20:16:19 +0900

    Impl:contact modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -39,13 +39,38 @@ console.log(`
 
 let is_twitter_visible = false;
 
-window.addEventListener("scroll", function() {
-	if (!is_twitter_visible) { 
-		show_twitter_icon(); 
+window.addEventListener("scroll", function () {
+	if (!is_twitter_visible) {
+		show_twitter_icon();
 		is_twitter_visible = true;
 	}
 });
 
 function show_twitter_icon() {
-	document.getElementById("twitter").classList.add("twitter-show");
+	document.getElementById("twitter").classList.add("show-twitter");
+}
+
+let is_contact_visible = false;
+
+function show_contact_modal() {
+	document.getElementById("contact-modal").classList.add("show-contact-modal");
+	document.getElementById("contact-modal").classList.remove("hide-contact-modal");
+	is_contact_visible = true;
+}
+
+function hide_contact_modal() {
+	document.getElementById("contact-modal").classList.add("hide-contact-modal");
+	document.getElementById("contact-modal").classList.remove("show-contact-modal");
+	is_contact_visible = false;
+
+}
+
+document.addEventListener("keydown", function (event) {
+	close_modal(event.key);
+});
+
+function close_modal(key) {
+	if (key == "Escape" && is_contact_visible) {
+		hide_contact_modal();
+	}
 }
