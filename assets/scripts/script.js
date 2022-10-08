11a1e7239ea9ed29bed2fa04f86b4cc0dcfe59de
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-10-09 05:27:36 +0900

    Improve:fmt

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -40,36 +40,36 @@ console.log(`
 let is_twitter_visible = false;
 
 window.addEventListener("scroll", function () {
-	if (!is_twitter_visible) {
-		show_twitter_icon();
-		is_twitter_visible = true;
-	}
+   if (!is_twitter_visible) {
+      show_twitter_icon();
+      is_twitter_visible = true;
+   }
 });
 
 function show_twitter_icon() {
-	document.getElementById("twitter").classList.add("show-twitter");
+   document.getElementById("twitter").classList.add("show-twitter");
 }
 
 let is_contact_visible = false;
 
 function show_contact_modal() {
-	document.getElementById("contact-modal").classList.add("show-contact-modal");
-	document.getElementById("contact-modal").classList.remove("hide-contact-modal");
-	is_contact_visible = true;
+   document.getElementById("contact-modal").classList.add("show-contact-modal");
+   document.getElementById("contact-modal").classList.remove("hide-contact-modal");
+   is_contact_visible = true;
 }
 
 function hide_contact_modal() {
-	document.getElementById("contact-modal").classList.add("hide-contact-modal");
-	document.getElementById("contact-modal").classList.remove("show-contact-modal");
-	is_contact_visible = false;
+   document.getElementById("contact-modal").classList.add("hide-contact-modal");
+   document.getElementById("contact-modal").classList.remove("show-contact-modal");
+   is_contact_visible = false;
 }
 
 document.addEventListener("keydown", function (event) {
-	close_modal(event.key);
+   close_modal(event.key);
 });
 
 function close_modal(key) {
-	if (key == "Escape" && is_contact_visible) {
-		hide_contact_modal();
-	}
+   if (key == "Escape" && is_contact_visible) {
+      hide_contact_modal();
+   }
 }
