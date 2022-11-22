d7e358b0024498a7469b9cecbf637535efaea649
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-11-23 07:32:57 +0900

    Improve:refactored contact modal impl

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,78 +12,79 @@ console.log(`
 
 /* Old version AA:
 console.log(`
-                                     -*ff
-                                            :f*++
-                              f              ffff=
-                          ffff
-                         ffffe   SSSSS
-                          eSSSSSSSSSSSSSSSS
-                 *        SSSSSSSSSSSSSSSSSSS
-                        SSSSSSSSS    .SSSSSSSSS
-                 +fffeSSSSSSS             SSSSSS:
-              .fffffeSSSSSSSe+             =SSSSSfff
-             =ffffffSSSSSSSfffeSSSSSSSS      SSefffff
-           ffffffffSSSSSSSf  SSSSSSSSSSSS    ffffffff*
-          fffffffffSSSSSSefeSSSSSSSSSSSSSS=   eeSeee       fffff
-           :ffffffeSSSSSSffSSSSSSS    SSSSS    SSSSS   *fffffffff
-              ffffeSSSSS  .SSSSSSf*   SSSSS=   SSSSS   .     ffff.
-                   SSSSS   *SSSSSefffffeSSSSffSSSSSS-         f   f
-                   SSSSS    SSSSSSSeeeffffeeffSSSSSSff             f
-                   =SSSSS    SSSSSSSSSSS:   feSSSSef
-     f        .     SSSSS     SSSSSSSSSSS    eeeffff             *
-                     SSSSS      eSefffffff-*fffffffff
-               fff   SSSSSSS       fffffffffffffffff.
-             fffffffffSSSSSSSS        fffffffffffff
-          fffffffffffffeSSSSSSSSS     SSeSS=+fff
-             fffffffffffffeeSSSSS    +SSSSS
-                  ffffffffffffffeSSSSSSSSS
-                      =ffffffffffffeeSS+
-                               ffffffff**
+									 -*ff
+											:f*++
+							  f              ffff=
+						  ffff
+						 ffffe   SSSSS
+						  eSSSSSSSSSSSSSSSS
+				 *        SSSSSSSSSSSSSSSSSSS
+						SSSSSSSSS    .SSSSSSSSS
+				 +fffeSSSSSSS             SSSSSS:
+			  .fffffeSSSSSSSe+             =SSSSSfff
+			 =ffffffSSSSSSSfffeSSSSSSSS      SSefffff
+		   ffffffffSSSSSSSf  SSSSSSSSSSSS    ffffffff*
+		  fffffffffSSSSSSefeSSSSSSSSSSSSSS=   eeSeee       fffff
+		   :ffffffeSSSSSSffSSSSSSS    SSSSS    SSSSS   *fffffffff
+			  ffffeSSSSS  .SSSSSSf*   SSSSS=   SSSSS   .     ffff.
+				   SSSSS   *SSSSSefffffeSSSSffSSSSSS-         f   f
+				   SSSSS    SSSSSSSeeeffffeeffSSSSSSff             f
+				   =SSSSS    SSSSSSSSSSS:   feSSSSef
+	 f        .     SSSSS     SSSSSSSSSSS    eeeffff             *
+					 SSSSS      eSefffffff-*fffffffff
+			   fff   SSSSSSS       fffffffffffffffff.
+			 fffffffffSSSSSSSS        fffffffffffff
+		  fffffffffffffeSSSSSSSSS     SSeSS=+fff
+			 fffffffffffffeeSSSSS    +SSSSS
+				  ffffffffffffffeSSSSSSSSS
+					  =ffffffffffffeeSS+
+							   ffffffff**
 
  ###                   ###     #####                               #
 #   #                 #   #    #                                   #
 #       ###   # ###  ####      #       ####  # ##    ###   # ###  ####    ####
  ###   #   #  ##      #        #####  #      ##  #  #   #  ##      #     #    
-    #   ####  #       #        #       ###   ##  #  #   #  #       #      ###
+	#   ####  #       #        #       ###   ##  #  #   #  #       #      ###
 #   #  #   #  #       #        #          #  # ##   #   #  #       #         #
  ###    ####  #       #        #####  ####   #       ###   #        ##   ####
-                                             #
+											 #
 `);
 */
 
 let is_twitter_visible = false;
 
 window.addEventListener("scroll", function () {
-   if (!is_twitter_visible) {
-      show_twitter_icon();
-      is_twitter_visible = true;
-   }
+	if (!is_twitter_visible) {
+		show_twitter_icon();
+		is_twitter_visible = true;
+	}
 });
 
 function show_twitter_icon() {
-   document.getElementById("twitter").classList.add("show-twitter");
+	document.getElementById("twitter").classList.add("show-twitter");
 }
 
 let is_contact_visible = false;
 
-function show_contact_modal() {
-   document.getElementById("contact-modal").classList.add("show-contact-modal");
-   document.getElementById("contact-modal").classList.remove("hide-contact-modal");
-   is_contact_visible = true;
-}
-
-function hide_contact_modal() {
-   document.getElementById("contact-modal").classList.add("hide-contact-modal");
-   document.getElementById("contact-modal").classList.remove("show-contact-modal");
-   is_contact_visible = false;
+function toggle_contact_modal() {
+	let cm_cl = document.getElementById("contact-modal").classList;
+	if (is_contact_visible == true) {
+		cm_cl.add("hide-contact-modal");
+		cm_cl.remove("show-contact-modal");
+		is_contact_visible = false;
+	} else {
+		cm_cl.add("show-contact-modal");
+		cm_cl.remove("hide-contact-modal");
+		is_contact_visible = true;
+	}
 }
 
 document.addEventListener("keydown", function (event) {
-   close_modal(event.key);
+	close_modal(event.key);
 });
 
 function close_modal(key) {
-   if (key == "Escape" && is_contact_visible) {
-      hide_contact_modal();
-   }
+	if (key == "Escape" && is_contact_visible) {
+		toggle_contact_modal();
+	}
 }
