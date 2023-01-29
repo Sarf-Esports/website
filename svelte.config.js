0d679b70858fa67b41fdaa44d8984d64bad9b277
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-01-29 22:15:32 +0900

    Improve:fmt

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import preprocess from "svelte-preprocess"
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
