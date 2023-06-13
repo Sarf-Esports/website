81efe20cd40e2a09819a7ca73279168831545ce6
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-06-13 15:40:21 +0900

    🍱 Add: new news article

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -24,6 +24,11 @@
 export const news_list = [
 	{
 		published: true,
+		date: '20230613',
+		title: 'APEX部門に新メンバー加入'
+	},
+	{
+		published: true,
 		date: '20230611',
 		title: 'フォートナイト部門に新メンバー加入'
 	},
