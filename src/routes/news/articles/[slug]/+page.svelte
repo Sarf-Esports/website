09c3f616b97dc9ca689f3df4cfa0a63bc15870ea
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-30 01:08:00 +0900

    🌐 L10n: localize date format

--- src/routes/news/articles/[slug]/+page.svelte
+++ src/routes/news/articles/[slug]/+page.svelte
@@ -1,8 +1,9 @@
 <!-- © 2022 - 2023 REVATI -->
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { fmt_date } from '$lib/util';
 	import { _ } from 'svelte-i18n';
+
+	import NewsDate from '$lib/components/news_date.svelte';
 
 	export let data: PageData;
 	const metadata = data.frontmatter;
@@ -27,7 +28,7 @@
 		<div id="content">
 			<img src={thumbnail_path} alt="thumbnail" />
 			<h1>{metadata.title}</h1>
-			<h2>{fmt_date(metadata.date)}</h2>
+			<h2><NewsDate date={metadata.date} /></h2>
 			<hr />
 			<article><svelte:component this={data.component} /></article>
 			<a href="/news"
