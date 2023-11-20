46196f3a4ee3dbad87c3b1728f25c34cb3bb7ee9
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-21 03:06:38 +0900

    ✨ Add: news article 20231120

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -24,6 +24,11 @@
 export const news_list = [
 	{
 		published: true,
+		date: '20231120',
+		title: 'Apex部門-ex加入のお知らせ'
+	},
+	{
+		published: true,
 		date: '20231029',
 		title: 'Overwatch部門-An2,KORPPIN,Witty脱退のお知らせ'
 	},
