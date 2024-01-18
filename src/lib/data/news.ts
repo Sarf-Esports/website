ade1aeeb7db3ab27a54c089f4cc3b560a44da79e
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-01-18 20:38:39 +0900

    ✨ Add: article 20240118

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -24,6 +24,11 @@
 export const news_list = [
 	{
 		published: true,
+		date: '20240118',
+		title: 'ストリーマー部門- 碧海まっちゃ 加入のお知らせ'
+	},
+	{
+		published: true,
 		date: '20240117',
 		title: 'ストリーマー部門-きむにぃ脱退のお知らせ'
 	},
