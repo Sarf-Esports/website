1f47a2d5258b1c16865f46e0897fa17e6f53c963
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-02-19 07:12:28 +0900

    ♻️ Improve: function `load` of `[slug]/+page.ts`

--- src/routes/news/articles/[slug]/+page.ts
+++ src/routes/news/articles/[slug]/+page.ts
@@ -1,15 +1,24 @@
 // © 2022 REVATI
 
 import type { PageLoad } from './$types';
+import type { SvelteComponent } from 'svelte';
+import type { ArticleMetadata } from '$lib/types';
 import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params }) => {
-	try {
-		const { default: component, metadata: frontmatter } = await import(
-			`../../../../../articles/${params.slug}.md`
-		);
-		return { component, frontmatter };
-	} catch {
-		throw error(404, 'This article is not found');
-	}
+export const load: PageLoad = async ({
+	params
+}): Promise<{
+	component: typeof SvelteComponent;
+	frontmatter: ArticleMetadata;
+}> => {
+	const { default: component, metadata: frontmatter } = await import(
+		`../../../../../articles/${params.slug}.md`
+	).catch(err);
+	if (!frontmatter.published) err();
+
+	return { component, frontmatter };
 };
+
+function err() {
+	throw error(404);
+}
