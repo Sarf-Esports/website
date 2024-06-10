1cffe4b3069b6727853e81be0b5c951c87d69716
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-06-10 22:54:36 +0900

    ♻️ Improve: SPONSORS section is now component [#93]

---
+++ src/routes/Sponsors.svelte
@@ -0,0 +1,24 @@
+<!-- © 2022 REVATI -->
+<script lang="ts">
+    import LinkButton from '$lib/components/LinkButton.svelte';
+</script>
+
+<a href="https://gachisup.com" target="_blank" rel="noopener noreferrer">
+    <img src="/images/logos/gachisup_oxipng.png" alt="" />
+    <LinkButton text="ガチサプ ONLINE STORE" />
+</a>
+
+<style lang="scss">
+    a {
+        display: block;
+        font-size: 22px;
+        text-decoration: none;
+    }
+
+    img {
+        display: block;
+        max-width: 86%;
+        margin: 0 auto;
+        margin-bottom: 40px;
+    }
+</style>
