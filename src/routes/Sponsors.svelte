9f85e4518353826838915d120d234f75c58fcfef
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-06-12 21:21:59 +0900

    🛠️ Update: text and its design of SPONSORS section items [#93]

--- src/routes/Sponsors.svelte
+++ src/routes/Sponsors.svelte
@@ -1,16 +1,16 @@
 <!-- © 2022 REVATI -->
 <script lang="ts">
+	import MaterialIcon from '$lib/components/MaterialIcon.svelte';
 	import { SPONSORS } from '$lib/data/sponsors';
-	import LinkButton from '$lib/components/LinkButton.svelte';
 </script>
 
 {#if 0 < SPONSORS.length}
 	<ul>
-		{#each SPONSORS as { name, website, logo }}
+		{#each SPONSORS as { name, url, logo }}
 			<li>
-				<a href={website.url} target="_blank" rel="noopener noreferrer">
+				<a href={url} target="_blank" rel="noopener noreferrer">
 					<img src="/images/logos/{logo}" alt={name} />
-					<LinkButton text={website.name} />
+					<span>{name}<MaterialIcon kind="box-arrow-up-right" width="16px" /></span>
 				</a>
 			</li>
 		{/each}
@@ -26,19 +26,22 @@
 	}
 
 	li {
-		margin: 64px 0;
+		margin: 0;
+		margin-bottom: 64px;
 	}
 
 	a {
-		display: block;
-		font-size: 22px;
-		text-decoration: none;
+		text-decoration-color: white;
 	}
 
 	img {
 		display: block;
 		max-width: 86%;
 		margin: 0 auto;
-		margin-bottom: 40px;
+		margin-bottom: 22px;
+	}
+
+	span {
+		font-size: 16px;
 	}
 </style>
