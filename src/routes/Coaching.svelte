dd362a7c81d99d4b4b4bdb5413e34189ae889fde
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-08-09 14:51:33 +0900

    🚧 Wip: add buttons

--- src/routes/Coaching.svelte
+++ src/routes/Coaching.svelte
@@ -5,12 +5,43 @@
 
 <img src="/images/coaching-poster.webp" alt={$_('coaching.poster')} />
 
-<p>内容。</p>
+<p>説明ほへとちりぬるをわかよたれそつねならむ</p>
+
+<div>
+	<button>料金表</button><button>コーチ一覧</button>
+</div>
+<div>
+	<a href="/" target="_blank" rel="noopener noreferrer">
+		応募フォームはこちら
+	</a>
+</div>
 
 <style lang="scss">
+	@use '$lib/stylesheets/variables/mixin' as *;
+
 	img {
 		width: 100%;
-		margin: 30px auto;
+		margin-top: 16px;
 		border-radius: 2px;
+	}
+
+	p {
+		margin-top: 20px !important;
+	}
+
+	div {
+		display: flex;
+		justify-content: center;
+		gap: 120px;
+	}
+
+	button {
+		margin: 28px 0;
+
+		@include btn(24px);
+	}
+
+	a {
+		font-size: 22px;
 	}
 </style>
