68644de4d0d1bc07ec948087a2ea0383a8c0d459
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-08-09 18:42:56 +0900

    🚧 Wip: add 2 modals

--- src/routes/Coaching.svelte
+++ src/routes/Coaching.svelte
@@ -1,19 +1,48 @@
 <!-- © 2022 REVATI -->
 <script lang="ts">
+	import Modal from '$lib/components/Modal.svelte';
+
 	import { _ } from 'svelte-i18n';
 	import { isFeesModalOpened, isCoachesModalOpened } from '$lib/scripts/stores';
 </script>
 
 <img src="/images/coaching-poster.webp" alt={$_('coaching.poster')} />
 
-<p>説明ほへとちりぬるをわかよたれそつねならむ</p>
+<p class="desc">説明ほへとちりぬるをわかよたれそつねならむ</p>
 
 <div>
 	<button on:click={
 		() => isFeesModalOpened.update(() => true)
-	}>料金表</button><button on:click={
+	}>料金表</button>{#if $isFeesModalOpened}<Modal title="料金表">
+		<table>
+			<tr>
+				<th>項目</th>
+				<th>料金</th>
+			</tr>
+			<tr>
+				<td>ここに</td>
+				<td>料金表を</td>
+			</tr>
+			<tr>
+				<td>置く</td>
+				<td>予定</td>
+			</tr>
+			<tr>
+				<td>コーチング</td>
+				<td>4,096 jpy</td>
+			</tr>
+			<tr>
+				<td>こんな</td>
+				<td>ふうに</td>
+			</tr>
+		</table>
+	</Modal>{/if}<button on:click={
 		() => isCoachesModalOpened.update(() => true)
-	}>コーチ一覧</button>
+	}>コーチ一覧</button>{#if $isCoachesModalOpened}<Modal title="コーチ一覧">
+		<table>
+			ここにコーチ一覧を置く予定
+		</table>
+	</Modal>{/if}
 </div>
 <div>
 	<a href="/" target="_blank" rel="noopener noreferrer">応募フォームはこちら</a>
@@ -28,7 +57,7 @@
 		border-radius: 2px;
 	}
 
-	p {
+	.desc {
 		margin-top: 20px !important;
 	}
 
@@ -42,6 +71,11 @@
 		margin: 28px 0;
 
 		@include btn(24px);
+	}
+
+	table {
+		margin: 0 auto;
+		font-size: 22px;
 	}
 
 	a {
