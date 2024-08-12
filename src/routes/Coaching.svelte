f43229040beb4e644f5fb718011fc978c858a0aa
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-08-12 14:16:01 +0900

    ✨ Add: Fees Modal

--- src/routes/Coaching.svelte
+++ src/routes/Coaching.svelte
@@ -13,34 +13,56 @@
 <div>
 	<button on:click={
 		() => isFeesModalOpened.update(() => true)
-	}>料金表</button>{#if $isFeesModalOpened}<Modal title="料金表">
+	}>料金表</button>{#if $isFeesModalOpened}<Modal title="料金表" minWidth={485}>
 		<table>
 			<tr>
-				<th>項目</th>
+				<th colspan="2">個人で受ける場合</th>
+			</tr>
+			<tr>
+				<th>プラン</th>
 				<th>料金</th>
 			</tr>
 			<tr>
-				<td>ここに</td>
-				<td>料金表を</td>
+				<td>通常プラン</td>
+				<td>3,000円 / 1時間</td>
 			</tr>
 			<tr>
-				<td>置く</td>
-				<td>予定</td>
+				<td>3回まとめてプラン</td>
+				<td>7,500円 / 3時間<span>(1時間あたり500円お得)</span></td>
 			</tr>
 			<tr>
-				<td>コーチング</td>
-				<td>4,096 jpy</td>
+				<td>1ヶ月定額プラン</td>
+				<td>16,000円 / 1ヶ月<span>(1時間あたり1,000円お得)</span></td>
+			</tr>
+		</table>
+		<table>
+			<tr>
+				<th colspan="2">チーム単位で受ける場合</th>
 			</tr>
 			<tr>
-				<td>こんな</td>
-				<td>ふうに</td>
+				<th>プラン</th>
+				<th>料金</th>
+			</tr>
+			<tr>
+				<td>通常プラン</td>
+				<td>6,000円 / 1時間</td>
+			</tr>
+			<tr>
+				<td>3回まとめてプラン</td>
+				<td>15,000円 / 3時間<span>(1時間あたり1,000円お得)</span></td>
+			</tr>
+			<tr>
+				<td>1ヶ月定額プラン</td>
+				<td>32,000円 / 1ヶ月<span>(1時間あたり2,000円お得)</span></td>
 			</tr>
 		</table>
 	</Modal>{/if}<button on:click={
 		() => isCoachesModalOpened.update(() => true)
 	}>コーチ一覧</button>{#if $isCoachesModalOpened}<Modal title="コーチ一覧">
 		<table>
-			ここにコーチ一覧を置く予定
+			<tr>
+				<td>ここにコーチ一覧を置く予定</td>
+			</tr>
 		</table>
 	</Modal>{/if}
 </div>
@@ -74,8 +96,44 @@
 	}
 
 	table {
-		margin: 0 auto;
-		font-size: 22px;
+		margin: 26px auto;
+		font-size: 21px;
+		border-collapse: collapse;
+		border: 2px solid #ccfbff98;
+	}
+
+	tr {
+		background-color: #122026;
+
+		&:first-child {
+			background-color: #121326;
+			font-weight: 900;
+			font-size: 20px;
+
+			th {
+				padding: 10px 16px;
+			}
+		}
+
+		&:hover td {
+			background-color: #ffffff10;
+		}
+	}
+
+	th, td {
+		padding: 5px 16px;
+	}
+
+	th {
+		font-weight: 900;
+		background-color: #121826;
+	}
+
+	td {
+		span {
+			display: block;
+			font-size: 16px;
+		}
 	}
 
 	a {
