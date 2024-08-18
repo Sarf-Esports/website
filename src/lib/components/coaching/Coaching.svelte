1cc490ede7436f736ab912dea9bfdb25afbcf153
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-08-18 15:49:54 +0900

    ✨ Add: X.com link of coaching project

--- src/lib/components/coaching/Coaching.svelte
+++ src/lib/components/coaching/Coaching.svelte
@@ -11,6 +11,8 @@
 		fees: $_('coaching.fees'),
 		coaches: $_('coaching.coaches')
 	};
+
+	const X_HANDLE = 'NGRev_coaching';
 </script>
 
 <img src="/images/coaching-poster.webp" alt={$_('w.poster')} />
@@ -29,6 +31,22 @@
 		{#if $isFeesModalOpened}
 			<Modal title={modalTitles.fees} minWidth={485} doesNotHaveBloom><Fees /></Modal>
 		{/if}
+	</div>
+	<div>
+		<a
+			href="https://x.com/{X_HANDLE}"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="no-after-icn"
+		>
+			<img
+				src="/images/logos/x_logo-white.png"
+				alt="X"
+				title="@{X_HANDLE}"
+				loading="lazy"
+				class="social-logo"
+			/>
+		</a>
 	</div>
 	<div>
 		<button on:click={() => isCoachesModalOpened.update(() => true)}>{modalTitles.coaches}</button>
@@ -64,13 +82,21 @@
 	.buttons {
 		display: flex;
 		justify-content: center;
-		gap: 120px;
+
+		div {
+			min-width: 172px;
+		}
 	}
 
 	button {
 		margin: 28px 0;
 
 		@include btn(24px);
+	}
+
+	.social-logo {
+		width: 38px;
+		margin-top: 37px;
 	}
 
 	a {
@@ -82,8 +108,20 @@
 			gap: 16px;
 		}
 
+		.buttons {
+			gap: 40px;
+
+			div {
+				min-width: initial;
+			}
+		}
+
 		button {
-			@include btn(22px);
+			@include btn(18px);
+		}
+
+		.social-logo {
+			width: 24px;
 		}
 
 		a {
