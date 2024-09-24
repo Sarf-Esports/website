e610dab7283fa777576c98ba97dd443506bc2cff
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-09-25 04:23:12 +0900

    ✨ Add: intro animation to modal [#154]

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,6 +1,7 @@
 <!-- © 2022 REVATI -->
 <script lang="ts">
 	import type { Writable } from 'svelte/store';
+	import { fly } from 'svelte/transition';
 
 	export let open: Writable<boolean>;
 	export let title: string | null = null;
@@ -37,29 +38,31 @@
 	class="modal"
 	style="min-width: min({minWidth}px, 100vw - 8px);"
 >
-	<div class="modal-content">
-		{#if title !== null}
-			<h1>{title}</h1>
-		{/if}
-		<slot />
-		<button on:click={close}>
-			<!--
-				Google Material Symbols and Icons - Close
-				https://fonts.google.com/icons?selected=Material+Symbols+Outlined:close:FILL@0;wght@400;GRAD@200;opsz@24&icon.query=close&icon.size=24&icon.color=%23e8eaed
-				This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
-			-->
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				height="24px"
-				viewBox="0 -960 960 960"
-				width="24px"
-				fill="#e8eaed"
-				><path
-					d="m252-176-74-76 227-228-227-230 74-76 229 230 227-230 74 76-227 230 227 228-74 76-227-230-229 230Z"
-				/></svg
-			>
-		</button>
-	</div>
+	{#if $open}
+		<div class="modal-content" transition:fly|global={{ y: -64, duration: 240 }}>
+			{#if title !== null}
+				<h1>{title}</h1>
+			{/if}
+			<slot />
+			<button on:click={close}>
+				<!--
+					Google Material Symbols and Icons - Close
+					https://fonts.google.com/icons?selected=Material+Symbols+Outlined:close:FILL@0;wght@400;GRAD@200;opsz@24&icon.query=close&icon.size=24&icon.color=%23e8eaed
+					This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
+				-->
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					height="24px"
+					viewBox="0 -960 960 960"
+					width="24px"
+					fill="#e8eaed"
+					><path
+						d="m252-176-74-76 227-228-227-230 74-76 229 230 227-230 74 76-227 230 227 228-74 76-227-230-229 230Z"
+					/></svg
+				>
+			</button>
+		</div>
+	{/if}
 </dialog>
 
 <style lang="scss">
