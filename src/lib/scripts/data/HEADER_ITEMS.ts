f8d4e5d46a6243f727f3719605dc6656b854dc0a
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-10-22 23:20:21 +0900

    ✨ Add: GOODS section to header [#164]

--- src/lib/scripts/data/HEADER_ITEMS.ts
+++ src/lib/scripts/data/HEADER_ITEMS.ts
@@ -3,5 +3,5 @@
 // If updated the `HEADER_ITEMS` JS constant, also adjust and update following variables:
 // - `$bp-hamburger-btn` Sass variable in `/src/lib/stylesheets/variables/_mixin.scss`
 // - `BREAKPOINT_HB` JS constant in `/src/lib/scripts/variables.ts`
-export const HEADER_ITEMS = ['about', 'news', 'teams', 'coaching', 'sponsors', 'contact'];
+export const HEADER_ITEMS = ['about', 'news', 'teams', 'coaching', 'goods', 'sponsors', 'contact'];
 export const NON_SECTION_ITEMS = ['contact'];
