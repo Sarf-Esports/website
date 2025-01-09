907166cecd5e5cf2c715678d41a72e7c394c821f
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-01-09 21:27:40 +0900

    🐛 Fix: "self-styled" text was not displaying when specified age and birthyear are provided

--- src/lib/components/home/teams/MemberCard.svelte
+++ src/lib/components/home/teams/MemberCard.svelte
@@ -46,7 +46,7 @@
 			{#if age === null && birthday !== null && birthday.year !== null}
 				{calcAge(new Date(birthday.year, birthday.month - 1, birthday.day))}
 			{:else if age !== null}
-				{age + (birthday !== null && birthday.year === null ? ` (${$_('w.selfStyled')})` : '')}
+				{age + ` (${$_('w.selfStyled')})`}
 			{:else}
 				<MaterialIcon {...LOCK_ICON} />
 			{/if}
