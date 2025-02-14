ba40d89f4f7f96e68f2f8736f401cffc1fc0b4fe
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-02-14 21:38:18 +0900

    ♿ A11y: add `id` attributes to each privacy policy section heading

--- src/routes/privacy/policy.svelte
+++ src/routes/privacy/policy.svelte
@@ -10,7 +10,7 @@
 		{$_('privacy.policy.desc.1')}
 	</p>
 
-	<h2>1. {$_('privacy.policy.1.0')}</h2>
+	<h2 id="policy1">1. {$_('privacy.policy.1.0')}</h2>
 	<p>{$_('privacy.policy.1.1')}</p>
 	<ul>
 		<li>{$_('privacy.policy.1.2')}</li>
@@ -23,7 +23,7 @@
 		{$_('privacy.policy.1.6')}
 	</p>
 
-	<h2>2. {$_('privacy.policy.2.0')}</h2>
+	<h2 id="policy2">2. {$_('privacy.policy.2.0')}</h2>
 	<p>{$_('privacy.policy.2.1')}</p>
 	<ul>
 		<li>{$_('privacy.policy.2.2')}</li>
@@ -32,7 +32,7 @@
 		<li>{$_('privacy.policy.2.5')}</li>
 	</ul>
 
-	<h2>3. {$_('privacy.policy.3.0')}</h2>
+	<h2 id="policy3">3. {$_('privacy.policy.3.0')}</h2>
 	<p>{$_('privacy.policy.3.1')}</p>
 	<ul>
 		<li>{$_('privacy.policy.3.2')}</li>
@@ -40,7 +40,7 @@
 		<li>{$_('privacy.policy.3.4')}</li>
 	</ul>
 
-	<h2>4. {$_('privacy.policy.4.0')}</h2>
+	<h2 id="policy4">4. {$_('privacy.policy.4.0')}</h2>
 	<p>{$_('privacy.policy.4.1')}</p>
 	<ul>
 		<li>{$_('privacy.policy.4.2')}</li>
@@ -48,7 +48,7 @@
 	</ul>
 	<p>{$_('privacy.policy.4.4')}</p>
 
-	<h2>5. {$_('privacy.policy.5.0')}</h2>
+	<h2 id="policy5">5. {$_('privacy.policy.5.0')}</h2>
 	<p>{$_('privacy.policy.5.1')}</p>
 	<ul>
 		<li>{$_('privacy.policy.5.2')}</li>
@@ -56,7 +56,7 @@
 	</ul>
 	<p>{$_('privacy.policy.5.4')}</p>
 
-	<h2>6. {$_('privacy.policy.6.0')}</h2>
+	<h2 id="policy6">6. {$_('privacy.policy.6.0')}</h2>
 	<p>{$_('privacy.policy.6.1')}<br /><br /></p>
 	<p>
 		{$_('privacy.policy.6.2')}
@@ -67,10 +67,10 @@
 		>{$_('privacy.policy.6.6')}
 	</p>
 
-	<h2>7. {$_('privacy.policy.7.0')}</h2>
+	<h2 id="policy7">7. {$_('privacy.policy.7.0')}</h2>
 	<p>{$_('privacy.policy.7.1')}</p>
 
-	<h2>8. {$_('privacy.policy.8.0')}</h2>
+	<h2 id="policy8">8. {$_('privacy.policy.8.0')}</h2>
 	<p>
 		{$_('privacy.policy.8.1')}<br />{$_('privacy.policy.8.2')}:
 		<a href="mailto:SOCIALS.email">{SOCIALS.email}</a>
