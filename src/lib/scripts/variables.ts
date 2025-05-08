9f6991f4977e8270f5e755349a6c0e7794e4eade
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-05-08 13:47:18 +0900

    ⚗️💩 Experim: change site URL to dev env to check OGP image [#234]
    ⚠️ Be sure to revert to the original URL before merging into the master branch.⚠️

--- src/lib/scripts/variables.ts
+++ src/lib/scripts/variables.ts
@@ -17,8 +17,10 @@ export const SOCIALS = {
 	github: 'Sarf-Esports'
 };
 
+// TMP: The site URL is set to the development URL to check the new OGP image.
+//      Be sure to revert to the original URL before merging into the master branch.
 /** URL of the site. */
-export const SITE_URL = 'https://revati.jp';
+export const SITE_URL = 'https://dev.revati.pages.dev'; // 'https://revati.jp';
 
 /**
  * The part of the full title of the pages.
